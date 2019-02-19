f44442cc0adde386f41659e3ae1e3fdbe42405af
mskian <veerfun23@gmail.com>
2019-02-19 13:06:22 +0530

    Add Requests Validation

--- torip.py
+++ torip.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 
+import sys
 import requests
 
-def get_tor_session():
-    session = requests.session()
-    # Tor uses the 9050 port as the default socks port
-    session.proxies = {'http':  'socks5://127.0.0.1:9050',
-                       'https': 'socks5://127.0.0.1:9050'}
-    return session
+# Create the session and set the proxies.
+proxies = {'http': 'socks5://127.0.0.1:9050',
+           'https': 'socks5://127.0.0.1:9050'}
+
+s = requests.Session()
+s.proxies = proxies
 
 # Make a request through the Tor connection
-session = get_tor_session()
-print('IP Given by TOR')
-print(session.get("https://api.ipify.org/").text)
+try:
+    r = s.get('https://api.ipify.org/')
+except requests.ConnectionError as e:
+    print("OOPS!! Connection Error - May be Tor is Not Enabled")
+except requests.Timeout as e:
+    print("OOPS!! Timeout Error")
+except requests.RequestException as e:
+    print("OOPS!! General Error")
+except KeyboardInterrupt:
+    print("Ok ok, quitting")
+    sys.exit(1)
+else:
+    print('IP Given by TOR')
+    print(r.text)
 
-# Following prints your normal public IP
-print('Your Public IP')
-print(requests.get("https://api.ipify.org/").text)
+    print('Your Public IP')
+    print(requests.get("https://api.ipify.org/").text)
