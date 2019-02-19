7ce3cbe39bc37f74bf7d15823b377e80a38a7fd2
mskian <veerfun23@gmail.com>
2019-02-19 17:01:51 +0530

    Add halo Spinner

--- tor.py
+++ tor.py
@@ -1,6 +1,8 @@
 import sys
+import time
 import requests
 from bs4 import BeautifulSoup
+from halo import Halo
 
 # Create the session and set the proxies.
 proxies = {'http': 'socks5://127.0.0.1:9050',
@@ -9,19 +11,46 @@ proxies = {'http': 'socks5://127.0.0.1:9050',
 s = requests.Session()
 s.proxies = proxies
 
+# Terminal Spinner
+spinner = Halo(text='Loading', color='green', spinner='hamburger')
+
 try:
+    spinner.start()
+    time.sleep(2)
+    spinner.text = 'Verifying the Connections'
+    ## Proxy
     r = s.get('https://check.torproject.org/')
+    spinner.succeed('It works!')
+    spinner.stop()
 except requests.ConnectionError as e:
+    spinner.start()
+    time.sleep(2)
+    spinner.color = 'red'
+    spinner.text = 'URL Error - Empty URL or Wrong URL'
+    spinner.color = 'red'
+    time.sleep(2)
+    spinner.fail('URL Validation Error - May be Tor is Not Enabled')
+    spinner.stop()
     print("OOPS!! Connection Error.")
-    regular = requests.get('https://check.torproject.org/')
-    BS = BeautifulSoup(regular.text, "html.parser")
+    ## Normal Request
+    p = requests.get('https://check.torproject.org/')
+    BS = BeautifulSoup(p.text, "html.parser")
     METATAG = BS.select('h1.off')[0].text.strip()
     print(METATAG)
 except requests.Timeout as e:
     print("OOPS!! Timeout Error")
 except requests.RequestException as e:
+    spinner.start()
+    time.sleep(2)
+    spinner.color = 'red'
+    spinner.text = 'Wrong URL or Empty Field'
+    spinner.color = 'red'
+    time.sleep(2)
+    spinner.fail('Wrong URL or Empty Field')
+    spinner.stop()
     print("OOPS!! General Error")
-except KeyboardInterrupt:
+except (KeyboardInterrupt, SystemExit):
+    spinner.stop()
     print("Ok ok, quitting")
     sys.exit(1)
 else:
--- torfind.py
+++ torfind.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
-
 import sys
+import time
 import requests
+from halo import Halo
 
 # Create the session and set the proxies.
 proxies = {'http': 'socks5://127.0.0.1:9050',
@@ -10,17 +10,42 @@ proxies = {'http': 'socks5://127.0.0.1:9050',
 s = requests.Session()
 s.proxies = proxies
 
+# Terminal Spinner
+spinner = Halo(text='Loading', color='green', spinner='hamburger')
+
 try:
     ## User input
-    LINK = input('\033[1;32mEnter a URL: = \033[1;m')
+    LINK = input('Enter a URL: ')
+    spinner.start()
+    time.sleep(2)
+    spinner.text = 'Reading the Output'
     r = s.get(LINK)
+    spinner.succeed('Successfully Fetched the URL')
+    spinner.stop()
 except requests.ConnectionError as e:
-    print("OOPS!! Connection Error")
+    spinner.start()
+    time.sleep(2)
+    spinner.color = 'red'
+    spinner.text = 'URL Error - Empty URL or Wrong URL'
+    spinner.color = 'red'
+    time.sleep(2)
+    spinner.fail('URL Validation Error')
+    spinner.stop()
+    print("OOPS!! Connection Error - May be Tor is Not Enabled or Can't Bypass them")
 except requests.Timeout as e:
     print("OOPS!! Timeout Error")
 except requests.RequestException as e:
+    spinner.start()
+    time.sleep(2)
+    spinner.color = 'red'
+    spinner.text = 'Wrong URL or Empty Field'
+    spinner.color = 'red'
+    time.sleep(2)
+    spinner.fail('Wrong URL or Empty Field')
+    spinner.stop()
     print("OOPS!! General Error (Enter a Valid URL) - Add HTTP/HTTPS infront of the URL")
-except KeyboardInterrupt:
+except (KeyboardInterrupt, SystemExit):
+    spinner.stop()
     print("Ok ok, quitting")
     sys.exit(1)
 else:
